848c182ad4ac880b46e4e98cb29f761c4524b2e9
eeedddiii <eeedddiii82@gmail.com>
2025-05-16 02:43:14 +0900

    feat: Update OAuth2SuccessHandler

--- src/main/java/asia/canopy/tree/config/OAuth2SuccessHandler.java
+++ src/main/java/asia/canopy/tree/config/OAuth2SuccessHandler.java
@@ -46,7 +46,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                 .build().toUriString();
 
         if (!userPrincipal.getUser().isProfileComplete()) {
-            redirectUrl = UriComponentsBuilder.fromUriString("https://www.canopy.asia/select-avatar")
+            redirectUrl = UriComponentsBuilder.fromUriString("https://www.canopy.asia/select-character")
                     .queryParam("token", token)
                     .build().toUriString();
         }
